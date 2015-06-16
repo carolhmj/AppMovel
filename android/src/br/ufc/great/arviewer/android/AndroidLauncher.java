456764d00b7bf4b0d00dfa44be8bcede1370e4d5
Luís Fernando Maia <luisfmss@Luiss-MacBook-Air.local>
2015-06-16 19:09:19 -0300

    Initial import

---
+++ android/src/br/ufc/great/arviewer/android/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package br.ufc.great.arviewer.android;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import br.ufc.great.arviewer.ARViewer;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new ARViewer(), config);
+	}
+}
