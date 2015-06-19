bb444b41b22629a3f23f495f53296bfbf675cb8c
luisfmss <luissantos@great.ufc.br>
2015-06-19 11:04:02 -0300

    Integrating Libgdx and CameraPreview

--- android/src/br/ufc/great/arviewer/android/AndroidLauncher.java
+++ android/src/br/ufc/great/arviewer/android/AndroidLauncher.java
@@ -1,16 +1,135 @@
 package br.ufc.great.arviewer.android;
 
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.graphics.PixelFormat;
+import android.hardware.Camera;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.SurfaceView;
+import android.view.View;
+import android.widget.FrameLayout;
 
 import com.badlogic.gdx.backends.android.AndroidApplication;
 import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+
 import br.ufc.great.arviewer.ARViewer;
 
-public class AndroidLauncher extends AndroidApplication {
-	@Override
-	protected void onCreate (Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
-		initialize(new ARViewer(), config);
-	}
+public class AndroidLauncher extends AndroidApplication implements LocationListener {
+
+    private String TAG = "ARViewer";
+
+    private Camera mCamera;
+    private CameraPreview mCameraPreview;
+    private View libgdxView;
+    final double lat_obj = -3.73422104;
+    final double lon_obj = -38.5482861;
+    private ARViewer arViewer;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+//		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+//		initialize(new ARViewer(), config);
+        setContentView(R.layout.activity_main);
+
+        AndroidApplicationConfiguration cfg = new AndroidApplicationConfiguration();
+        cfg.r = 8;
+        cfg.g = 8;
+        cfg.b = 8;
+        cfg.a = 8;
+        arViewer = new ARViewer();
+        libgdxView = initializeForView(arViewer, cfg);
+
+        if (libgdxView instanceof SurfaceView) {
+            SurfaceView glView = (SurfaceView) libgdxView;
+            // force alpha channel - I'm not sure we need this as the GL surface is already using alpha channel
+            glView.getHolder().setFormat(PixelFormat.TRANSLUCENT);
+        }
+
+        FrameLayout frame = (FrameLayout) findViewById(R.id.camera);
+        frame.addView(libgdxView);
+
+        if (checkCameraHardware(getApplicationContext())) {
+            mCamera = getCameraInstance();
+            mCameraPreview = new CameraPreview(this, mCamera);
+            frame.addView(mCameraPreview);
+        } else {
+            finish();
+        }
+
+        ((LocationManager) getSystemService(Context.LOCATION_SERVICE)).requestLocationUpdates(
+                LocationManager.GPS_PROVIDER, 100, 0.01f, this);
+    }
+
+    /**
+     * Check if this device has a camera
+     */
+    private boolean checkCameraHardware(Context context) {
+        if (context.getPackageManager().hasSystemFeature(PackageManager.FEATURE_CAMERA)) {
+            // this device has a camera
+            return true;
+        } else {
+            // no camera on this device
+            return false;
+        }
+    }
+
+    private static Camera getCameraInstance() {
+        Camera c = null;
+        try {
+            c = Camera.open(); // attempt to get a Camera instance
+        } catch (Exception e) {
+            // Camera is not available (in use or does not exist)
+        }
+        return c; // returns null if camera is unavailable
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        mCamera.stopPreview();
+        mCamera.release();
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+        Log.e(TAG, "latitude: " + location.getLatitude()
+                + " , longitude: " + location.getLongitude() + " , altitude: "
+                + location.getAltitude());
+
+        float[] dist = new float[1];
+
+        double lat = location.getLatitude();
+        double lon = location.getLongitude();
+
+        Location.distanceBetween(lat, lon, lat_obj, lon_obj, dist);
+        float distance = dist[0];
+        lat = lat - lat_obj;
+        lon = lon - lon_obj;
+        float xcoord=(float)((distance*lat)/(lat+lon));
+        float ycoord=(float)((distance*lon)/(lat+lon));
+
+        arViewer.setCamCoord(xcoord,ycoord);
+        Log.e(TAG, "distance: " + dist[0]);
+        Log.e(TAG, "x,y: " + xcoord+" , "+ycoord);
+    }
+
+    @Override
+    public void onStatusChanged(String provider, int status, Bundle extras) {
+
+    }
+
+    @Override
+    public void onProviderEnabled(String provider) {
+
+    }
+
+    @Override
+    public void onProviderDisabled(String provider) {
+
+    }
 }
--- core/src/br/ufc/great/arviewer/ARViewer.java
+++ core/src/br/ufc/great/arviewer/ARViewer.java
@@ -2,18 +2,17 @@ package br.ufc.great.arviewer;
 
 import com.badlogic.gdx.ApplicationListener;
 import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.assets.AssetManager;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.PerspectiveCamera;
-import com.badlogic.gdx.graphics.VertexAttributes.Usage;
 import com.badlogic.gdx.graphics.g3d.Environment;
-import com.badlogic.gdx.graphics.g3d.Material;
 import com.badlogic.gdx.graphics.g3d.Model;
 import com.badlogic.gdx.graphics.g3d.ModelBatch;
 import com.badlogic.gdx.graphics.g3d.ModelInstance;
 import com.badlogic.gdx.graphics.g3d.attributes.ColorAttribute;
 import com.badlogic.gdx.graphics.g3d.environment.DirectionalLight;
-import com.badlogic.gdx.graphics.g3d.utils.ModelBuilder;
+import com.badlogic.gdx.math.Vector3;
+import com.badlogic.gdx.utils.Array;
 
 public class ARViewer implements ApplicationListener {
     public Environment environment;
@@ -22,9 +21,18 @@ public class ARViewer implements ApplicationListener {
     public ModelBatch modelBatch;
     public Model model;
     public ModelInstance instance;
+    public AssetManager assets;
+    public Array<ModelInstance> instances = new Array<ModelInstance>();
+
     float azimuth;
     float pitch;
     float roll;
+    boolean init = true;
+    float mina, maxa, minp, maxp, minr, maxr;
+    boolean loading;
+    float alpha = 0.01f;
+    float xcam, ycam;
+    float coordAlpha = 2f;
 
     @Override
     public void create() {
@@ -35,31 +43,49 @@ public class ARViewer implements ApplicationListener {
         modelBatch = new ModelBatch();
 
         cam = new PerspectiveCamera(67, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
-        cam.position.set(10f, 10f, 10f);
+//        cam.position.set(3f, 0, 0);
+        cam.position.set(2f, 0, 0);
         cam.lookAt(0, 0, 0);
-        cam.near = 1f;
+        cam.near = 0.001f;
         cam.far = 300f;
         cam.update();
 
-        ModelBuilder modelBuilder = new ModelBuilder();
-        model = modelBuilder.createBox(5f, 5f, 5f,
-                new Material(ColorAttribute.createDiffuse(Color.GREEN)),
-                Usage.Position | Usage.Normal);
-        instance = new ModelInstance(model);
+        assets = new AssetManager();
+        assets.load("ship.obj", Model.class);
+        loading = true;
+//        ModelBuilder modelBuilder = new ModelBuilder();
+//        model = modelBuilder.createBox(5f, 5f, 5f,
+//                new Material(ColorAttribute.createDiffuse(Color.GREEN)),
+//                Usage.Position | Usage.Normal);
+//        instance = new ModelInstance(model);
 
 //        camController = new CameraInputController(cam);
 //        Gdx.input.setInputProcessor(camController);
+        mina = minp = minr = 999;
+        maxa = maxp = maxr = -999;
+    }
+
+
+    private void doneLoading() {
+        Model ship = assets.get("ship.obj", Model.class);
+        ModelInstance shipInstance = new ModelInstance(ship);
+        instances.add(shipInstance);
+        loading = false;
     }
 
     @Override
     public void render() {
+
 //        camController.update();
+        if (loading && assets.update())
+            doneLoading();
 
         Gdx.gl.glViewport(0, 0, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT | GL20.GL_DEPTH_BUFFER_BIT);
+        Gdx.gl.glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
 
         modelBatch.begin(cam);
-        modelBatch.render(instance, environment);
+        modelBatch.render(instances, environment);
         modelBatch.end();
 
 
@@ -67,21 +93,84 @@ public class ARViewer implements ApplicationListener {
         float devPitch = Gdx.input.getPitch();
         float devRoll = Gdx.input.getRoll();
 
-        Gdx.app.error("az - roll - pitch", String.valueOf(azimuth) + " - " + String.valueOf(roll) + " - " + String.valueOf(pitch));
-        cam.rotate(azimuth - devAzimuth, 0, 0, 1);
-        //cam.rotate(roll - devRoll, 0, 1, 0);
-        cam.rotate(pitch - devPitch, 0, 1, 0);
+        if (devAzimuth > maxa) maxa = devAzimuth;
+        if (devPitch > maxp) maxp = devPitch;
+        if (devRoll > maxr) maxr = devRoll;
 
+        if (devAzimuth < mina) mina = devAzimuth;
+        if (devPitch < minp) minp = devPitch;
+        if (devRoll < minr) minr = devRoll;
+
+//        if (init) {
+//            Gdx.app.error("init", " apr ");
+//            azimuth = devAzimuth;
+//            pitch = devPitch;
+//            roll = devRoll;
+//            init = false;
+//        }
+
+//        float devAzimuth = 0;
+//        float devPitch = 0;
+//        float devRoll = 0;
+
+
+//        devAzimuth = azimuth + 0.01f * (azimuth - devAzimuth);
+//        devPitch = pitch + 0.01f * (pitch - devPitch);
+//        devRoll = roll + 0.01f * (roll - devRoll);
+
+        if (Gdx.input.justTouched()) {
+            //init = true;
+            Gdx.app.error("azimuth - pitch - roll: ", String.valueOf(azimuth) + " | " + String.valueOf(pitch) + " | " + String.valueOf(roll));
+            Gdx.app.error("maxa", String.valueOf(maxa));
+            Gdx.app.error("mina", String.valueOf(mina));
+            Gdx.app.error("maxp", String.valueOf(maxp));
+            Gdx.app.error("minp", String.valueOf(minp));
+            Gdx.app.error("maxr", String.valueOf(maxr));
+            Gdx.app.error("minr", String.valueOf(minr));
+            cam.position.set(3, 0, 3);
+            init = false;
+        }
+
+        cam.direction.x = -1;
+        cam.direction.y = 0;
+        cam.direction.z = 0;
+        cam.up.x = 0;
+        cam.up.y = 0;
+        cam.up.z = -1;
+
+//        azimuth = devAzimuth;
+//        pitch = devPitch;
+//        roll = devRoll;
+
+        azimuth = azimuth + alpha * (devAzimuth - azimuth);
+        pitch = pitch + alpha * (devPitch - pitch);
+        roll = roll + alpha * (devRoll - roll);
+
+        //1 proprio eixo(azimuth), 2 cima(pitch) , 3 esq->direira (roll)
+//        Gdx.app.error("azimuth - roll - pitch", String.valueOf(azimuth) + " - " + String.valueOf(roll) + " - " + String.valueOf(pitch));
+        //cam.rotate(-devAzimuth, 0, 0, 1);
+        //cam.rotate(devPitch*2f, 0, 1, 0);
+        //cam.rotate(devRoll, 0, 1, 0);//roll
+
+        //Vector3 look = new Vector3(pitch, azimuth, (roll - 90));
+        Vector3 look = new Vector3(0, 0, 0);
+        if (init) {
+            cam.position.set(xcam / coordAlpha, 0, ycam / coordAlpha);
+            Gdx.app.error("Arviewer", "x,y: " + xcam / coordAlpha + " , " + ycam / coordAlpha);
+        }
+        cam.lookAt(look);
         cam.update();
 
-        azimuth = devAzimuth;
-        pitch = devPitch;
-        roll = devRoll;
-
-        if (Gdx.input.justTouched())
-            cam.lookAt(1, 0, 0);
+//        if (Gdx.input.justTouched())
+//            cam.lookAt(0, 0, 0);
 
     }
+
+    public void setCamCoord(float x, float y) {
+        xcam = x;
+        ycam = y;
+    }
+
 
     @Override
     public void dispose() {
